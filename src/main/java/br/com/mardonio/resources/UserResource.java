bb0778bdae46582f4adbaf6af3c4dc58b88e73e8
Mardoniosc <mardonio@live.com>
2020-10-03 22:56:34 -0300

    :v: Usando padrão DTO para retornar usuários

--- src/main/java/br/com/mardonio/resources/UserResource.java
+++ src/main/java/br/com/mardonio/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.mardonio.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mardonio.domain.User;
+import br.com.mardonio.dto.UserDTO;
 import br.com.mardonio.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> lista = service.findAll();
-		return ResponseEntity.ok().body(lista);
+		List<UserDTO> listDTO = lista.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
