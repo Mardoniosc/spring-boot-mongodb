2737ff3dcbb7d6785af9ad96e0366938dfeccaa4
Mardoniosc <mardonio@live.com>
2020-10-04 11:46:29 -0300

    :v: Obtendo um usuário por id

--- src/main/java/br/com/mardonio/services/UserService.java
+++ src/main/java/br/com/mardonio/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.mardonio.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.mardonio.domain.User;
 import br.com.mardonio.repository.UserRepository;
+import br.com.mardonio.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
