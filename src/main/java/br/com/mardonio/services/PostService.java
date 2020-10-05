e196827cdd52ab57a1054b43c5b694b19c5f0208
Mardoniosc <mardonio@live.com>
2020-10-05 09:26:48 -0300

    :v: Obtendo um post por id

---
+++ src/main/java/br/com/mardonio/services/PostService.java
@@ -0,0 +1,22 @@
+package br.com.mardonio.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.mardonio.domain.Post;
+import br.com.mardonio.repository.PostRepository;
+import br.com.mardonio.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
