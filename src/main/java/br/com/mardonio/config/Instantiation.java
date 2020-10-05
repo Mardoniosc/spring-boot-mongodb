513af2b93557e64842742e39cacd983cdbed4fde
Mardoniosc <mardonio@live.com>
2020-10-05 09:39:09 -0300

    :v: Acrescentando comentários aos posts

--- src/main/java/br/com/mardonio/config/Instantiation.java
+++ src/main/java/br/com/mardonio/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.mardonio.domain.Post;
 import br.com.mardonio.domain.User;
 import br.com.mardonio.dto.AuthorDTO;
+import br.com.mardonio.dto.CommentDTO;
 import br.com.mardonio.repository.PostRepository;
 import br.com.mardonio.repository.UserRepository;
 
@@ -37,8 +38,15 @@ public class Instantiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para são paulo abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei Feliz hoje!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu Viagem!", "Vou viajar para são paulo abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia!", "Acordei Feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa Viagem Mano!", sdf.parse("21/03/2020"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2020"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2020"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
